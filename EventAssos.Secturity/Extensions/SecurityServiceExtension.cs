94ef32cc3cac684c5a2b7e279dbcb53952ffaaed
fra <frarusso24@live.it>
2026-05-04 16:31:28 +0200

    Adding of JwtSettings, JwtService, JwtAuthenticationService

---
+++ EventAssos.Secturity/Extensions/SecurityServiceExtension.cs
@@ -0,0 +1,26 @@
+﻿using EventAssos.Core.Services.Tools;
+using EventAssos.Security.Services.Tools;
+using EventAssos.Security.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAssos.Security.Extensions
+{
+    public static class SecurityServiceExtension
+    {
+        public static void AddSecurityServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var jwtSettings = configuration
+            .GetSection("JwtSettings")
+            .Get<JwtSettings>();
+            services.AddSingleton(jwtSettings);
+
+            
+            services.AddScoped<IJwtService, JwtService>();
+        }
+    }
+}
