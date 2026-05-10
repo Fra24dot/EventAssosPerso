f5df51707f05d56d185a2baed029ae4d624d3729
fra <frarusso24@live.it>
2026-05-10 16:51:16 +0200

    Adding of email sender service, corspolicy and scalar

--- EventAssos.Api/Program.cs
+++ EventAssos.Api/Program.cs
@@ -1,6 +1,7 @@
 using EventAssos.Api.Extensions;
 using EventAssos.Security.Extensions;
 using EventAssos.Security.Services.Tools;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
@@ -8,8 +9,16 @@ builder.Services.AddSecurityServices(builder.Configuration);
 
 builder.Services.ConfigureJwTAuthentication(builder.Configuration);
 
-
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // L'URL d'Angular
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials(); // Important pour les tokens 
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -21,6 +30,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
--- EventAssos.Secturity/Services/Auth/AuthService.cs
+++ EventAssos.Secturity/Services/Auth/AuthService.cs
@@ -13,7 +13,8 @@ namespace EventAssos.Security.Services.Auth
 {
     public class AuthService(IUserRepository _userRepository,
         IPasswordHacherService _passwordHacherService,
-        IPasswordGeneratorService _passwordGeneratorService) : IAuthService
+        IPasswordGeneratorService _passwordGeneratorService,
+        IEmailService _emailService) : IAuthService
     {
         public async Task<User?> RegisterAsync(string email)
         {
@@ -36,7 +37,25 @@ namespace EventAssos.Security.Services.Auth
 
             };
 
-            return await _userRepository.AddAsync(user);
+            var newUser = await _userRepository.AddAsync(user);
+
+            string subject = "Bienvenue - Votre mot de passe provisoire";
+
+            //Corps du message 
+            string body = $@"
+            <html>
+                <body>
+                    <h2>Bienvenue sur EventAssos !</h2>
+                    <p>Votre compte a été créé avec succès.</p>
+                    <p>Votre identifiant: <strong>{user.Email}</strong></p>
+                    <p>Voici votre mot de passe provisoire : <strong>{generatedPassword}</strong></p>
+                    <p>Nous vous conseillons de le modifier dès votre première connexion.</p>
+                </body>
+            </html>";
+
+            await _emailService.SendEmailAsync(user.Email, subject, body);
+
+            return newUser;  
         }
     }
 }
